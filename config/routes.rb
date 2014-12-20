d50f32b1697acdd036f6e03065e7e89ce20fbdec
Abdullah <abd00s@users.noreply.github.com>
2014-12-20 17:56:54 -0500

    Define session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+
   resources :users, except: :index do
     resources :events, except: [:new, :edit, :show]
   end
+  resources :user_sessions, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
