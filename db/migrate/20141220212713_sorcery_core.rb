0bfd26ec56f4e05644bbfcb26aee4d29c5cd4d33
Abdullah <abd00s@users.noreply.github.com>
2014-12-20 16:36:16 -0500

    G User model with sorcery and migrate

---
+++ db/migrate/20141220212713_sorcery_core.rb
@@ -0,0 +1,16 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string  :first_name,       :null => false
+      t.string  :last_name,        :null => false
+      t.string  :email,            :null => false
+      t.string  :crypted_password, :null => false
+      t.string  :salt,             :null => false
+      t.integer :application,      { default: 1, null: false }
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220212040) do
+ActiveRecord::Schema.define(version: 20141220212713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,18 @@ ActiveRecord::Schema.define(version: 20141220212040) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name",                   null: false
+    t.string   "last_name",                    null: false
+    t.string   "email",                        null: false
+    t.string   "crypted_password",             null: false
+    t.string   "salt",                         null: false
+    t.integer  "application",      default: 1, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
