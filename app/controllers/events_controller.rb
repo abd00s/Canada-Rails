cacbfc505c0bcf99f3689bf49a1ab003c6647b7b
Abdullah <abd00s@users.noreply.github.com>
2014-12-20 21:04:43 -0500

    Add event handles abck to back absences

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,16 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.status = 2
+    @event.user = current_user
+    @prev = Event.where(user_id: current_user.id).order(:date_from).last
+    if @event.date_from == @prev.date_from
+      @prev.destroy
+    else
+      @prev.date_to = @event.date_from
+      @prev.save
+    end
+    @now = Event.create(date_from: @event.date_to, date_to: Date.today, location: "Canada", description: "Present", status: 1, user_id: current_user.id)
     if @event.save
       redirect_to user_events_path, notice: "Added"
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,20 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   validates :email, uniqueness: true
+
+  def absences
+    duration = 0
+    if self.events.where(status: 2)
+      self.events.where(status: 2).each {|e| duration += (e.date_to - e.date_from).to_i }
+      duration
+    else
+      0
+    end
+  end
+
+  def presence
+    duration = 0
+    self.events.where(status: 1).each {|e| duration += (e.date_to - e.date_from).to_i }
+    duration
+  end
 end
