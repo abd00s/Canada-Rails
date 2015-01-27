771840133638b89153d1d73c07e79785427e77cb
Abdullah <abd00s@users.noreply.github.com>
2015-01-27 12:22:52 -0500

    Add commentary outlining logic of events created upon sign up and events logged by user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,13 +7,26 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     @event.status = 2
     @event.user = current_user
+    # @prev selects the latest chronological event in the user's travel log
+    # This is always a "Present" event. 
     @prev = Event.where(user_id: current_user.id).order(:date_from).last
     if @event.date_from == @prev.date_from
+      # This condition corresponds to the user inserting an event that begins on 
+      # the same day another travel event ended (i.e. user has traveled to 
+      # different locations in the same absence period.)
       @prev.destroy
+      # The most recent "Present" event is deleted, to be replaced by the 
+      # new @now event
     else
+      # This condition corresponds to a stand-alone absence event (i.e. user
+      # traveled to one location only on same absence event.)
+      # The date_to of the latest "Present" event is updated to reflect the date
+      # user left Canada on the new event.
       @prev.date_to = @event.date_from
       @prev.save
     end
+    # @now is an automatically created event that reflects presence in Canada
+    # since date of return from the new event to present day.
     @now = Event.create(date_from: @event.date_to, date_to: Date.today, location: "Canada", description: "Present", status: 1, user_id: current_user.id)
     if @event.save
       redirect_to user_events_path, notice: "Added"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
+      # Upon signing up, the user's first Event entry is automatically populated
+      # This event starts at the user's "Landing Date" and ends Today, and has
+      # the status of 1 indicating presence in Canada
       Event.create(date_from: @user.start_date, date_to: Date.today, location: "Canada", description: "Present", status: 1, user_id: @user.id)
       redirect_to user_path(@user), notice: "Signed up successfully"
     else
